9f6563c44c9435f5e5b3a5e9260eeafc467ea741
Jellope <ryanbartoli707@gmail.com>
2017-09-12 17:42:01 -0700

    Added Alien Opponent

---
+++ A1Prj/src/com/mycompany/a1/Alien.java
@@ -0,0 +1,11 @@
+package com.mycompany.a1;
+
+public abstract class Alien extends Opponents {
+	
+	private void move(){
+		//Aliens move like dorks.
+	}
+	private void setColor(){
+		//Aliens don't change color;
+	}
+}
