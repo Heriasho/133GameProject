592e6afe30f05498c146301ae2dbf12715e7a289
Jellope <ryanbartoli707@gmail.com>
2017-09-12 17:04:04 -0700

    Added Rescurers GameObject

---
+++ A1Prj/src/com/mycompany/a1/Rescuers.java
@@ -0,0 +1,13 @@
+package com.mycompany.a1;
+
+public abstract class Rescuers extends GameObject {
+	
+	private int size;
+	
+	private void size(){
+		this.size = size*2;
+	}
+	private void setColor() {
+		
+	}
+}
