635ede5ca3bb01586e5a39b02fddea65ad842461
Jellope <ryanbartoli707@gmail.com>
2017-09-12 17:11:53 -0700

    Added Opponents GameObject

---
+++ A1Prj/src/com/mycompany/a1/Opponents.java
@@ -0,0 +1,11 @@
+package com.mycompany.a1;
+
+public abstract class Opponents extends GameObject {
+	private int size;
+	private int speed;
+	private int direction;
+	
+	private void guide() {
+		//Mute since Opponents do not use this.
+	}
+}
