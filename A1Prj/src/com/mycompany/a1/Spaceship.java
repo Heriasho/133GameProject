6934b504af13f364fabdb48b571e2e2c243954e5
Jellope <ryanbartoli707@gmail.com>
2017-09-12 17:06:56 -0700

    Added Spaceship Rescurer. Gave it the properties in Rescuer.

---
+++ A1Prj/src/com/mycompany/a1/Spaceship.java
@@ -0,0 +1,12 @@
+package com.mycompany.a1;
+
+public abstract class Spaceship extends Rescuers {
+	private int size;
+	
+	private void size(){
+		this.size = size*2;
+	}
+	private void setColor() {
+		
+	}
+}
