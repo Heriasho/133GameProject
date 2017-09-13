00a1f92b8813f0af89dd07715a1d93f89197d1f6
Jellope <ryanbartoli707@gmail.com>
2017-09-12 17:13:47 -0700

    Added Astronaut Opponent

---
+++ A1Prj/src/com/mycompany/a1/Astronaut.java
@@ -0,0 +1,9 @@
+package com.mycompany.a1;
+
+public abstract class Astronaut extends Opponents {
+	private int health;
+	
+	private void setcolor() {
+		//Astronauts change color as their health dips.
+	}
+}
